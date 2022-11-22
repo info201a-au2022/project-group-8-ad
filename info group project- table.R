816e54c70a95f02e2f4738b81ec47de2fd957098
Rachel <rmjoshua@uw.edu>
2022-11-21 17:34:25 -0800

    all tables uploaded, working on renaming columns if necessary

--- info group project- table.R
+++ info group project- table.R
@@ -3,15 +3,7 @@ library(tidyverse)
 
 
 
-#lowest_in_each_state <- counties %>% 
-#group_by(state)%>%
-#  filter(date == max(date, na.rm = TRUE)) %>% 
-#  filter(deaths == min(deaths, na.rm = TRUE)) %>% 
-#  pull (location)
-
-#View(lowest_in_each_state)
-
-
+#Co2 Dataset
 Co2_Dataset <- read.csv("/Users/rmjos/Downloads/AIRTRANS_CO2.csv")
 
 View(Co2_Dataset)
@@ -24,12 +16,33 @@ adjusted_co2 <- Co2_Dataset %>%
     Country, 
     Flight.type,
     TIME,
+    Pollutant,
     SOURCE,
-    Source.of.emissions
+    Source.of.emissions,
+    Value
     )
 
 View(adjusted_co2)
 
+
+
+
+#historical emissions data set
+historical_emissions <- read.csv("/Users/rmjos/Downloads/historical_emissions.csv")
+View(historical_emissions)
+
+
+
+adjusted_historical_emissions <- historical_emissions %>% 
+  group_by(Country) %>% 
+  select(-2, -3)
+
+View(adjusted_historical_emissions)
+
+
+
+
+#US passenger dataset
 passenger_dataset <- read.csv("/Users/rmjos/Downloads/International_Report_Passengers.csv")
 
 View(passenger_dataset)
@@ -37,12 +50,9 @@ View(passenger_dataset)
 
 passenger_dataset <- passenger_dataset %>% 
   rename(
-    "usg_apt_id" == "US Gateway Airport ID",
-    "usg_apt" == "Gateway Airport ID",
-    "usg_wac" == "World Gateway code"
+    "US_Gateway_Airport_ID" = usg_apt,
+    "World_Gateway_code" = usg_wac
   )
-
-
 
 
 adjusted_passenger_dataset <- passenger_dataset %>% 
@@ -56,10 +66,6 @@ adjusted_passenger_dataset <- passenger_dataset %>%
 
 View(adjusted_passenger_dataset)
 
-
-departure_dataset <- read.csv("/Users/rmjos/Downloads/International_Report_Departures.csv")
-
-View(departure_dataset)
 
 
 
