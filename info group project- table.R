a48a882044dfdf8316a7b3da1c099498c659ca98
mmk49 <mustafakhan524@yahoo.com>
2022-12-02 11:13:40 -0800

    added departures csv

--- info group project- table.R
+++ info group project- table.R
@@ -5,14 +5,14 @@ library(tidyverse)
 
 
 #Co2 Dataset
-Co2_Dataset <- read.csv("/Users/rmjos/Downloads/AIRTRANS_CO2.csv")
+Co2_Dataset <- read.csv("../data/AIRTRANS_CO2.csv")
 
 View(Co2_Dataset)
 
 Co2_Dataset <- Co2_Dataset %>% 
   rename("Year" = Time )
 
-View(Co2_Dataset)
+# View(Co2_Dataset)
 
 
 adjusted_co2 <- Co2_Dataset %>% 
@@ -30,18 +30,18 @@ adjusted_co2 <- Co2_Dataset %>%
     Value
   )
 
-View(adjusted_co2)
+# View(adjusted_co2)
 
 small_co2 <- head(adjusted_co2, 200)
 
-View(small_co2)
+# View(small_co2)
 
 
 
 #US passenger dataset
-passenger_dataset <- read.csv("/Users/rmjos/Downloads/International_Report_Passengers.csv")
+passenger_dataset <- read.csv("../data/International_Report_Passengers.csv")
 
-View(passenger_dataset)
+# View(passenger_dataset)
 
 
 passenger_dataset <- passenger_dataset %>% 
@@ -70,9 +70,9 @@ passenger_dataset_smol <- head(passenger_dataset_1, 200)
 
 #US Departures
 
-departure_dataset <- read.csv("/Users/rmjos/Downloads/International_Report_Departures.csv")
+departure_dataset <- read.csv("../data/International_Report_Departures.csv")
 
-View(departure_dataset)
+# View(departure_dataset)
 
 
 
@@ -99,7 +99,7 @@ View(departure_dataset_1)
 
 departure_dataset_smol <- head(departure_dataset_1, 200)
 
-View(departure_dataset_smol)
+# View(departure_dataset_smol)
 
 
 #combining
@@ -119,7 +119,7 @@ smol_all_combined <-
     by = "Year"
   )
 
-View(smol_all_combined)
+# View(smol_all_combined)
 
 
 
